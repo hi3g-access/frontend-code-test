d4afb7a7db08ecdce6aac579478a0917ab1c09ef
daniel-trevino <5331300+daniel-trevino@users.noreply.github.com>
2024-04-10 15:16:00 +0200

    feat: update fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -2,7 +2,7 @@ function fetcher<T>(route: string): Promise<T> {
   /* our token cookie gets sent with this request */
   return fetch(route)
     .then(r => r.ok && r.json())
-    .then(user => user || null)
+    .then(res => res || null)
 }
 
 export default fetcher
